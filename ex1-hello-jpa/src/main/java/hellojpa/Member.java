31b4837198230bab77132f636d412a91fdcbcdd1
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-03 23:46:37 +0900

    6-2

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -7,33 +7,7 @@ import javax.persistence.*;
 
 @Entity
 public class Member {
-//    @Id
-//    private Long id;
-//    private String name;
-//
-//    public Member(){
-//    }
-//    public Member(Long id, String name){
-//        this.id=id;
-//        this.name=name;
-//    }
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-    //---------------5-1--------------------
+
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
     private Long id;
@@ -41,37 +15,10 @@ public class Member {
     @Column(name="USERNAME")
     private String username;
 
-    //객체지향적X
-//    @Column(name="TEAM_ID")
-//    private Long teamId;
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getUsername() {
-//        return username;
-//    }
-//
-//    public void setUsername(String username) {
-//        this.username = username;
-//    }
-//
-//    public Long getTeamId() {
-//        return teamId;
-//    }
-//
-//    public void setTeamId(Long teamId) {
-//        this.teamId = teamId;
-//    }
-
-    //객체지향적
-    @ManyToOne //member입장에서 member가 N team이 1
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
+//    //일대다 양방향(공식적x)
+//    @ManyToOne
+//    @JoinColumn(name="TEAM_ID",insertable=false,updatable=false)//읽기 전용 매핑
+//    private Team team;
 
     public Long getId() {
         return id;
@@ -87,20 +34,5 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-//    public void setTeam(Team team) {
-//        this.team = team;
-//    }
-   // -------------------5-3------------------------
-    //5-2에서 setTeam만 바꾸기
-    //team.getMembers().add(member)를 까먹는 경우가 생기기 때문에 jpamain에서 안 쓸 경우 setTeam에 넣어주기
-    public void setTeam(Team team){
-        this.team=team;
-        team.getMembers().add(this);
     }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -7,36 +7,13 @@ import java.util.List;
 
 @Entity
 public class Team {
-//    @Id @GeneratedValue
-//    @Column(name="TEAM_ID")
-//    private Long id;
-//    private String name;
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-    //-----------------5-2----------------------
-    //양방향
     @Id @GeneratedValue
     @Column(name="TEAM_ID")
     private Long id;
     private String name;
 
-    //team에서 member로는 1대 N
-    @OneToMany(mappedBy="team")//member의 팀에 걸려있음(반대쪽)
+    @OneToMany
+    @JoinColumn(name="TEAM_ID") //없으면 중간테이블 생성됨
     private List<Member> members=new ArrayList<>();
 
     public Long getId() {
@@ -62,10 +39,5 @@ public class Team {
     public void setMembers(List<Member> members) {
         this.members = members;
     }
-    //--------------------------------5-3-------------------
 
-    public void addMember(Member member){
-        member.setTeam(this);
-        members.add(member);
-    }
 }

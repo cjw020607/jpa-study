bbbd9bdbed99037bb222656880122eeb70b53cf5
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-07 12:13:56 +0900

    8-3 cascade

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,13 +1,10 @@
 package hellojpa;
 
-import org.hibernate.Hibernate;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
+
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -18,31 +15,21 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team team=new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Member member=new Member();
-            member.setUsername("member");
-            member.setTeam(team);
-            em.persist(member);
+            Child child1=new Child();
+            Child child2=new Child();
 
-            em.flush();
-            em.clear();
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member m=em.find(Member.class,member.getId());
+            //원래 다 해줘야 함
+//            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2);
 
-            System.out.println("m="+m.getTeam().getClass()); //프록시 아닌 진짜
-
-            System.out.println("=============");
-            System.out.println("teamName="+m.getTeam().getName());//초기화 하지 않고 진짜 값 출력
-            System.out.println("=============");
-
-            //jpql에서 문제
-
-            //sql:select * from Member가 먼저 db에 나감
-            //member를 보니 team이 eager여서 sql: select * from Team where TEAM_ID=xxxx 나감
-//            List<Member> members=em.createQuery("select m from Member m",Member.class).getResultList();
+            //cascade 할 경우 parent만 해도 child persist됨
+            em.persist(parent);
 
             tx.commit();
         }catch(Exception e){
---
+++ ex1-hello-jpa/src/main/java/hellojpa/Parent.java
@@ -0,0 +1,38 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy="parent",cascade= CascadeType.ALL) //밑에 애들 다 persist 해줌
+    private List<Child> childList=new ArrayList<>();
+
+    public void addChild(Child child){
+        childList.add(child);
+        child.setParent(this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

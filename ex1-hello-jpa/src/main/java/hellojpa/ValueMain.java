f6a3d34b9a9a7207851687ff206959b5ed187ff7
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-10 15:08:23 +0900

    9-4 identity, equivalence

--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -2,26 +2,17 @@ package hellojpa;
 
 public class ValueMain {
     public static void main(String[] args){
-        //기본 타입
-//        int a=10;
-//        int b=a;//a 복사, 저장공간 따로 (기본 타입은 공유가 안됨)
-//
-//        a=20;
-//
-//        System.out.println("a="+a);
-//        System.out.println("b="+b);
-//        //결과 a=20, b=10
 
-        //래퍼 클래스, 특수 클래스는 공유 가능하지만 변경x
+        int a=10;
+        int b=10;
 
-        Integer a= 10;
-        Integer b=a; //복사x 주소값이 넘어감
+        System.out.println("a==b: "+(a==b)); //true
 
-        //만약 a를 20으로 바꿀 수 있다면 a,b 둘 다 20 출력
-        //하지만 변경 자체가 안되기 떄문에 부작용이 안일어남
+        Address address1= new Address("city","street","1000");
+        Address address2= new Address("city","street","1000");
 
-        System.out.println("a="+a);
-        System.out.println("b="+b);
+        System.out.println("adress1==address2: " +(address1==address2)); //false (참조값이 다름)
+        System.out.println("adress1 equals address2: " +(address1.equals(address2))); //override 하면 true
 
 
     }

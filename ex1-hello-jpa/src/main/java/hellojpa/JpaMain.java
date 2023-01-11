88853f2e6d9c2a05eb4470af7042e6f479556b03
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-11 14:59:27 +0900

    10-1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Set;
 
@@ -18,25 +21,58 @@ public class JpaMain {
 
         try{
 
-            //값타입 저장
+//            //엔티티 객체를 대상으로 쿼리
+//            List<Member> result=em.createQuery(
+//                    "select m From Member m where m.username like '%kim%'",
+//                    Member.class
+//            ).getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member);
+//
+//            }
+
+            //--------
+            //Criteria 사용 준비
+//            CriteriaBuilder cb=em.getCriteriaBuilder();
+//            CriteriaQuery<Member> query=cb.createQuery(Member.class);
+//
+//            Root<Member> m=query.from(Member.class);
+
+////            CriteriaQuery<Member> cq=query.select(m).where(cb.equal(m.get("username"),"kim"));
+////            List<Member> resultList=em.createQuery(cq).getResultList();
+
+//            //동적 쿼리 깔끔함
+//            CriteriaQuery<Member> cq=query.select(m);
+//            String username="dsafas";
+//            if(username!=null){
+//                cq=cq.where(cb.equal(m.get("username"),"kim"));
+//            }
+//            List<Member> resultList=em.createQuery(cq).getResultList();
+
+            //--------
+//            //네이티브 sql
+//            em.createNativeQuery("select MEMBER_ID,city,street,zipcode,USERNAME from MEMBER").getResultList();
+//            tx.commit();
+
             Member member=new Member();
             member.setUsername("member1");
-            member.setHomeAddress(new Address("homeCity","street","10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("족발");
-            member.getFavoriteFoods().add("피자");
-
-            member.getAddressHistory().add(new AddressEntity("old1","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street","10000"));
-
             em.persist(member);
-            //값 타입은 따로 persist 할 필요x member에 의존
 
-            em.flush();
-            em.clear();
+            //flush->commit,query
 
-            tx.commit();
+            //jpa 관련 (flush 됨)
+            List<Member>resultList=em.createNativeQuery("select MEMBER_ID,city,street,zipcode,USERNAME from MEMBER").getResultList();
+
+            // em.flush();
+            // jpa와 관련 없기 때문에 강제 플러시 필요
+            // 결과 0
+            //dbconn.executeQuery("select * from member");
+
+            for (Member member1 : resultList) {
+                System.out.println("member1="+member1);
+
+            }
         }catch(Exception e){
             tx.rollback();
             e.printStackTrace();

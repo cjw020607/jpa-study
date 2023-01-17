828fc5b98d48ce0af549fc9d7e4c959919839857
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-17 17:34:13 +0900

    10-3

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -23,29 +23,59 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-//            //반환 타입 명확할 떄
-//            TypedQuery<Member> query1=em.createQuery("select m from Member m",Member.class);
-//            TypedQuery<String> query2=em.createQuery("select m.username from Member m",String.class);
-//            //반환 타입 명확하지 않을 때
-//            Query query3=em.createQuery("select m.username, m.age from Member m");
+            em.flush();
+            em.clear();
 
+            //엔티티 프로젝션
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
+//
+//            Member findMember=result.get(0);
+//            findMember.setAge(20);
+
+            //---------
+            //엔티티 프로젝션
+//            //예측 어려움
+//            List<Team> result=em.createQuery("select m.team from Member m", Team.class)
+//                    .getResultList();
+            //예측 가능(join 명시하는 것이 좋음)
+//            List<Team> result=em.createQuery("select t from Member m join m.team t", Team.class)
+//                            .getResultList();
             //----------
-//            TypedQuery<Member> query=em.createQuery("select m from Member m",Member.class);
-//            //결과가 하나 이상일 때
-//            List<Member> resultList = query.getResultList();
-//            for (Member member1 : resultList) {
-//                System.out.println("member1 = " + member1);
-//            }
-////            //결과가 정확히 하나만 있어야 함
-////            Member result=query.getSingleResult();
-////            System.out.println("result= "+result);
+//            //임베디드 타입 프로젝션(어디 소속인지 엔티티를 정해줘야 함)
+//            em.createQuery("select o.address from Order o",Address.class)
+//                            .getResultList();
+            //---------
+//            //스칼라 타입 프로젝션
+//            em.createQuery("select m.username, m.age from Member m")
+//                    .getResultList();
             //----------
-            TypedQuery<Member> query=em.createQuery("select m from Member m where m.username=:username",Member.class);
-            query.setParameter("username","member1");
-            Member singleResult = query.getSingleResult();
-            System.out.println("singleResult= "+singleResult.getUsername());
+            //Query 타입으로 조회
+//            List resultList = em.createQuery("select m.username,m.age from Member m", Member.class)
+//                      .getResultList();
+//            Object o=resultList.get(0);
+//            Object[] result=(Object[]) o;
+//            System.out.println("username="+result[0]);
+//            System.out.println("age="+result[1]);
+            //---------
+            //Object[] 타입으로 조회
+//            List<Object[]> resultList = em.createQuery("select m.username,m.age from Member m")
+//                        .getResultList();
+//
+//            Object[] result=resultList.get(0);
+//            System.out.println("username="+result[0]);
+//            System.out.println("age="+result[1]);
+            //-------
+            //new 명령어로 조회
+            //마치 생성자 호출 하듯
+//            List<MemberDTO> result= em.createQuery("select new jpql.MemberDTO(m.username,m.age) from Member m",MemberDTO.class)
+//                          .getResultList();
+//
+//            MemberDTO memberDTO =result.get(0);
+//            System.out.println("memberDTO="+memberDTO.getUsername());
+//            System.out.println("memberDTO="+memberDTO.getAge());
 
-
+            tx.commit();
         }catch(Exception e){
             tx.rollback();
             e.printStackTrace();

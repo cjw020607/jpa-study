8d60566d0c9d49477a1edffbe7c16e4e22172cf8
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-27 22:51:57 +0900

    11-1

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -18,69 +19,62 @@ public class JpaMain {
 
         try{
 
+            Team team=new Team();
+            em.persist(team);
+
             Member member1=new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2=new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
 
             em.flush();
             em.clear();
 
-            //concat
-            //String query="select concat('a','b') From Member m";
-            //String query="select 'a' || 'b' From Member m";
-
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            //-------
-            //substring (몇 번째부터 몇 개 잘라냄)
-//            String query="select substring(m.username,2,3) From Member m";
-//
-//            List<String> result = em.createQuery(query, String.class)
-//                    .getResultList();
-//
-//            for (String s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            //-------
-            //locate (abcdefg에서 de 찾음)
-            //String query="select locate('de','abcdefg') From Member m";
-//            List<Integer> result = em.createQuery(query, Integer.class)
-//                    .getResultList();
-//
-//            for (Integer s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            //-------
-            //size (콜렉션의 크기)
-            //String query="select size(t.members) From Team t";
-//            List<Integer> result = em.createQuery(query, Integer.class)
-//                    .getResultList();
-//
-//            for (Integer s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            //---------
-            //사용자 정의 함수 호출
-            String query="select function('group_concat',m.username) From Member m";
-
+            //상태 필드 (경로 탐색의 끝이고 더이상 탐색 불가능)
+            String query="select m.username From Member m";
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
             for (String s : result) {
                 System.out.println("s = " + s);
             }
+
+            //-------
+            //단일 값 연관 경로 (묵시적 내부 조인 발생, 탐색 가능 ex) m.team.name)
+//            String query="select m.team From Member m";
+//
+//            List<Team> result = em.createQuery(query, Team.class)
+//                    .getResultList();
+//
+//            for (Team s : result) {
+//                System.out.println("s = " + s);
+//            }
+
+            //컬렉션 값 연관 경로 (t.members.size까진 가능)
+//            String query="select t.members From Team t";
+//            //ex)t.members.username이 불가능 하기 때문에 원하는 속성 가져올 때 명시적 조인 사용
+//            // query="select m.username From Team t join t.members m";
+//
+//            Collection result = em.createQuery(query, Collection.class)
+//                    .getResultList();
+//
+//            for (Object o : result) {
+//                System.out.println("o = " + o);
+//            }
+
+            //-------
+            //t.members.size
+//            String query="select t.members.size from Team t";
+//
+//            Integer result=em.createQuery(query, Integer.class)
+//                            .getSingleResult();
+//
+//            System.out.println("result = " + result);
 
             tx.commit();
         }catch(Exception e){

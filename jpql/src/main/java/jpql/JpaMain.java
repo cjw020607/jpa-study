d41ccd06c4d9789164bbfc64a7c9da675f52a4a8
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-01-31 14:10:58 +0900

    11-2 fetch join1

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -19,62 +19,70 @@ public class JpaMain {
 
         try{
 
-            Team team=new Team();
-            em.persist(team);
+            Team teamA=new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB=new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1=new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2=new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3=new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            //상태 필드 (경로 탐색의 끝이고 더이상 탐색 불가능)
-            String query="select m.username From Member m";
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
+//            String query="select m From Member m";
+//
+//            //fetch join
+//            //String query="select m From Member m join fetch m.team";
+//            //영속성 컨텍스트에 이미 다 있음. proxy가 아닌 실제 데이터
+//
+//
+//            List<Member> result=em.createQuery(query,Member.class)
+//                            .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername()+", "+member.getTeam().getName());
+//                //회원1, 팀A(SQL) 팀A가 영속성 컨텍스트에 없기 때문에 회원1 전에 쿼리 나감
+//                //회원2, 팀A(1차캐시) 팀A가 영속성 컨텍스트에 있기 때문에 쿼리 안나감
+//                //회원3, 팀B(SQL) 팀B가 영속성 컨텍스트에 없기 때문에 쿼리 나감
+//
+//                //회원 100명 -> N + 1 문제 =>join petch로 해결
+//            }
 
-            for (String s : result) {
-                System.out.println("s = " + s);
+            //---------------
+            //컬렉션 fetch join(연관된 엔티티도 함께 조회(즉시 로딩))
+            String query="select t From Team t join fetch t.members";
+
+            //중복 제거 (distinct)
+            //String query="select distinct t From Team t join fetch t.members";
+
+            //일반 조인 (연관된 엔티티 함께 조회하지 않음 (팀만 조회하고 회원 엔티티는 조회x))
+            //String query="select t From Team t join t.members m";
+
+            List<Team> result=em.createQuery(query, Team.class)
+                            .getResultList();
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName()+"|members="+team.getMembers().size());
+                for( Member member: team.getMembers()){
+                    System.out.println("->member = " + member);
+                }
             }
-
-            //-------
-            //단일 값 연관 경로 (묵시적 내부 조인 발생, 탐색 가능 ex) m.team.name)
-//            String query="select m.team From Member m";
-//
-//            List<Team> result = em.createQuery(query, Team.class)
-//                    .getResultList();
-//
-//            for (Team s : result) {
-//                System.out.println("s = " + s);
-//            }
-
-            //컬렉션 값 연관 경로 (t.members.size까진 가능)
-//            String query="select t.members From Team t";
-//            //ex)t.members.username이 불가능 하기 때문에 원하는 속성 가져올 때 명시적 조인 사용
-//            // query="select m.username From Team t join t.members m";
-//
-//            Collection result = em.createQuery(query, Collection.class)
-//                    .getResultList();
-//
-//            for (Object o : result) {
-//                System.out.println("o = " + o);
-//            }
-
-            //-------
-            //t.members.size
-//            String query="select t.members.size from Team t";
-//
-//            Integer result=em.createQuery(query, Integer.class)
-//                            .getSingleResult();
-//
-//            System.out.println("result = " + result);
 
             tx.commit();
         }catch(Exception e){

63002a916d44a376ac09e13e4c4435677ae3f01c
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-02-01 18:21:49 +0900

    11-7 bulk operation

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -29,32 +29,41 @@ public class JpaMain {
 
             Member member1=new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2=new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3=new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
+            //flush 자동 호출
+            //벌크 연산은 db에만 반영함
+            //영속성 컨텍스트에는 age 0인데 db는 age 20임
+            int resultCount=em.createQuery("update Member m set m.age=20")
+                            .executeUpdate();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            System.out.println("resultCount = " + resultCount);
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
-                    
+            System.out.println("member1.getAge() = "+member1.getAge());
+            System.out.println("member2.getAge() = "+member2.getAge());
+            System.out.println("member3.getAge() = "+member3.getAge());
+
+            //영속성 컨텍스트 초기화 후 db에서 깔끔하게 새로 가져옴
+//            em.clear();
+//            Member findMember=em.find(Member.class, member1.getId());
+//            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             tx.commit();
+
         }catch(Exception e){
             tx.rollback();
             e.printStackTrace();

41288213c1786acdd1bed6eaf4bf3d1ed3eaf56a
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-02-07 15:09:21 +0900

    2-1-3

--- jpashop2/src/main/java/jpabook/Application.java
+++ jpashop2/src/main/java/jpabook/Application.java
@@ -8,11 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		Hello hello=new Hello();
-		hello.setData("Hello");
-		String data=hello.getData();
-		System.out.println("data = " + data);
-		
 		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ jpashop2/src/main/java/jpabook/HelloController.java
@@ -0,0 +1,17 @@
+package jpabook;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        //data인 키에 hello!!!값을 전달해줌
+        model.addAttribute("data","hello!!!");
+        //spring boot가 자동으로 hello.html해줌
+        return "hello";
+    }
+}

85de8743a8cb878a51a417ab7cb70367a23be4ec
cjw020607 <80667141+cjw020607@users.noreply.github.com>
2023-03-01 23:54:59 +0900

    2-2-5

--- jpashop2/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop2/src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,14 +17,23 @@ public class Order {
     @Column(name="order_id")
     private Long id;
 
-    @ManyToOne
+    //x to one 의 기본형은 EAGER
+    @ManyToOne(fetch=FetchType.LAZY)
     @JoinColumn(name="member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade=CascadeType.ALL)
     private List<OrderItem> orderItems=new ArrayList<>();
 
-    @OneToOne
+    //cascade 없을 때
+    //persist(orderItemA)
+    //persist(orderItemB)
+    //persist(order)
+
+    //cascade 있을 때
+    //persist(order)만 하면 됨 (cascade가 persist를 전파함)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    //order만 persist하면 delivery도 같이 persist됨
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -33,5 +42,28 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [order, cancel]
 
+    //연관관계 편의 메서드(양방향일때 쓰면 좋음)
+    public void setMember(Member member){
+        this.member=member;
+        member.getOrders().add(this);
+    }
 
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery=delivery;
+        delivery.setOrder(this);
+    }
+
+//    public static void main(String[] args){
+//        Member member=new Member();
+//        Order order = new Order();
+//
+//        //연관관계 편의 매서드 쓰면 이 한줄이 줄어듦
+//        //member.getOrders().add(order);
+//        order.setMember(member);
+//    }
 }
